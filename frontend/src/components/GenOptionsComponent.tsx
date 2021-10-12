2e5ca2b6979993ddb2120e69d39a2ead120d7d6c
Magnus Lykke Dahlø <magnusld@stud.ntnu.no>
2021-10-12 09:55:23 +0200

    feat(#2): legge til styling til genoptions

--- frontend/src/components/GenOptionsComponent.tsx
+++ frontend/src/components/GenOptionsComponent.tsx
@@ -8,61 +8,61 @@ export function GenOptionsComponent() {
 
   return (
       <div style={{display: "flex", justifyContent: "center"}}>
-          <div className={"genOptions"}>
-              <div>
-                  <h3>Generasjon</h3>
-              </div>
-              <div className={"showGenSelection"}>
-                  <ToggleButton className="mb-2"
-                                type="checkbox" value="1"
-                                checked={checked}
-                                variant="outline-primary"
-                                id="toggle-check"
-                                onClick={() => setChecked(!checked)}>Gen 1</ToggleButton>
-                  <ToggleButton className="mb-2"
-                                type="checkbox" value="1"
-                                checked={checked}
-                                variant="outline-primary"
-                                id="toggle-check"
-                                onClick={() => setChecked(!checked)}>Gen 2</ToggleButton>
-                  <ToggleButton className="mb-2"
-                                type="checkbox" value="1"
-                                checked={checked}
-                                variant="outline-primary"
-                                id="toggle-check"
-                                onClick={() => setChecked(!checked)}>Gen 3</ToggleButton>
-                  <ToggleButton className="mb-2"
-                                type="checkbox" value="1"
-                                checked={checked}
-                                variant="outline-primary"
-                                id="toggle-check"
-                                onClick={() => setChecked(!checked)}>Gen 4</ToggleButton>
-                  <ToggleButton className="mb-2"
-                                type="checkbox" value="1"
-                                checked={checked}
-                                variant="outline-primary"
-                                id="toggle-check"
-                                onClick={() => setChecked(!checked)}>Gen 5</ToggleButton>
-                  <ToggleButton className="mb-2"
-                                type="checkbox" value="1"
-                                checked={checked}
-                                variant="outline-primary"
-                                id="toggle-check"
-                                onClick={() => setChecked(!checked)}>Gen 6</ToggleButton>
-                  <ToggleButton className="mb-2"
-                                type="checkbox" value="1"
-                                checked={checked}
-                                variant="outline-primary"
-                                id="toggle-check"
-                                onClick={() => setChecked(!checked)}>Gen 7</ToggleButton>
-                  <ToggleButton className="mb-2"
-                                type="checkbox" value="1"
-                                checked={checked}
-                                variant="outline-primary"
-                                id="toggle-check"
-                                onClick={() => setChecked(!checked)}>Gen 8</ToggleButton>
-              </div>
+        <div className={"genOptions"}>
+          <div>
+            <h3 style={{textAlign: "left"}}>Generasjon</h3>
           </div>
+          <div className={"showGenSelection"}>
+            <ToggleButton className="mb-2"
+                          type="checkbox" value="1"
+                          checked={checked}
+                          variant="outline-primary"
+                          id="toggle-check"
+                          onClick={() => setChecked(!checked)}>Gen 1</ToggleButton>
+            <ToggleButton className="mb-2"
+                          type="checkbox" value="1"
+                          checked={checked}
+                          variant="outline-primary"
+                          id="toggle-check"
+                          onClick={() => setChecked(!checked)}>Gen 2</ToggleButton>
+            <ToggleButton className="mb-2"
+                          type="checkbox" value="1"
+                          checked={checked}
+                          variant="outline-primary"
+                          id="toggle-check"
+                          onClick={() => setChecked(!checked)}>Gen 3</ToggleButton>
+            <ToggleButton className="mb-2"
+                          type="checkbox" value="1"
+                          checked={checked}
+                          variant="outline-primary"
+                          id="toggle-check"
+                          onClick={() => setChecked(!checked)}>Gen 4</ToggleButton>
+            <ToggleButton className="mb-2"
+                          type="checkbox" value="1"
+                          checked={checked}
+                          variant="outline-primary"
+                          id="toggle-check"
+                          onClick={() => setChecked(!checked)}>Gen 5</ToggleButton>
+            <ToggleButton className="mb-2"
+                          type="checkbox" value="1"
+                          checked={checked}
+                          variant="outline-primary"
+                          id="toggle-check"
+                          onClick={() => setChecked(!checked)}>Gen 6</ToggleButton>
+            <ToggleButton className="mb-2"
+                          type="checkbox" value="1"
+                          checked={checked}
+                          variant="outline-primary"
+                          id="toggle-check"
+                          onClick={() => setChecked(!checked)}>Gen 7</ToggleButton>
+            <ToggleButton className="mb-2"
+                          type="checkbox" value="1"
+                          checked={checked}
+                          variant="outline-primary"
+                          id="toggle-check"
+                          onClick={() => setChecked(!checked)}>Gen 8</ToggleButton>
+          </div>
+        </div>
       </div>
   )
 }
