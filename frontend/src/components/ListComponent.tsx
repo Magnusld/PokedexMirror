70ebeab221c8b27a078e41dbad039a87e996258b
Magnus Lykke Dahlø <magnusld@stud.ntnu.no>
2021-10-18 14:00:52 +0200

    feat(#6): Legge til listekomponent, gjør dog ingenting enda

---
+++ frontend/src/components/ListComponent.tsx
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+import '../style/App.css';
+
+export function ListComponent(props: {
+  asGrid: boolean
+}) {
+
+  return (
+      <div>
+
+      </div>
+  )
+}
