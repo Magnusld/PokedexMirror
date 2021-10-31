5f5347264178c595e9f3b4d531cb74a533d38981
magnsskj <magnsskj@stud.ntnu.no>
2021-10-31 13:56:04 +0100

    feat(#25): pokemon bilde og ikoner infoside

--- frontend/src/pages/Info.tsx
+++ frontend/src/pages/Info.tsx
@@ -6,6 +6,26 @@ import { useParams } from 'react-router-dom';
 import gql from 'graphql-tag';
 import { useQuery } from '@apollo/client';
 import { PokemonAdvanced } from '../types';
+import { getFullSize } from '../util/imageResolver';
+
+import {ReactComponent as Bug} from "../assets/typeIconsSvg/Pokémon_Bug_Type_Icon.svg";
+import {ReactComponent as Dark} from "../assets/typeIconsSvg/Pokémon_Dark_Type_Icon.svg";
+import {ReactComponent as Dragon} from "../assets/typeIconsSvg/Pokémon_Dragon_Type_Icon.svg";
+import {ReactComponent as Electric} from "../assets/typeIconsSvg/Pokémon_Electric_Type_Icon.svg";
+import {ReactComponent as Fairy} from "../assets/typeIconsSvg/Pokémon_Fairy_Type_Icon.svg";
+import {ReactComponent as Fighting} from "../assets/typeIconsSvg/Pokémon_Fighting_Type_Icon.svg";
+import {ReactComponent as Fire} from "../assets/typeIconsSvg/Pokémon_Fire_Type_Icon.svg";
+import {ReactComponent as Flying} from "../assets/typeIconsSvg/Pokémon_Flying_Type_Icon.svg";
+import {ReactComponent as Ghost} from "../assets/typeIconsSvg/Pokémon_Ghost_Type_Icon.svg";
+import {ReactComponent as Grass} from "../assets/typeIconsSvg/Pokémon_Grass_Type_Icon.svg";
+import {ReactComponent as Ground} from "../assets/typeIconsSvg/Pokémon_Ground_Type_Icon.svg";
+import {ReactComponent as Ice} from "../assets/typeIconsSvg/Pokémon_Ice_Type_Icon.svg";
+import {ReactComponent as Normal} from "../assets/typeIconsSvg/Pokémon_Normal_Type_Icon.svg";
+import {ReactComponent as Poison} from "../assets/typeIconsSvg/Pokémon_Poison_Type_Icon.svg";
+import {ReactComponent as Psychic} from "../assets/typeIconsSvg/Pokémon_Psychic_Type_Icon.svg";
+import {ReactComponent as Rock} from "../assets/typeIconsSvg/Pokémon_Rock_Type_Icon.svg";
+import {ReactComponent as Steel} from "../assets/typeIconsSvg/Pokémon_Steel_Type_Icon.svg";
+import {ReactComponent as Water} from "../assets/typeIconsSvg/Pokémon_Water_Type_Icon.svg";
 
 function Info() {
 
@@ -81,6 +101,29 @@ function Info() {
         return typeList;
     }
 
+    let showTypeEmblem = typesToList(data ? data.pokemon.type1 : "", data ? data.pokemon.type2 : "").map((Type) => (
+        <span>
+          {Type === "Bug" ? <Bug className={"typeIcon"}/> : null}
+          {Type === "Dark" ? <Dark className={"typeIcon"}/> : null}
+          {Type === "Dragon" ? <Dragon className={"typeIcon"}/> : null}
+          {Type === "Electric" ? <Electric className={"typeIcon"}/> : null}
+          {Type === "Fairy" ? <Fairy className={"typeIcon"}/> : null}
+          {Type === "Fighting" ? <Fighting className={"typeIcon"}/> : null}
+          {Type === "Fire" ? <Fire className={"typeIcon"}/> : null}
+          {Type === "Flying" ? <Flying className={"typeIcon"}/> : null}
+          {Type === "Ghost" ? <Ghost className={"typeIcon"}/> : null}
+          {Type === "Grass" ? <Grass className={"typeIcon"}/> : null}
+          {Type === "Ground" ? <Ground className={"typeIcon"}/> : null}
+          {Type === "Ice" ? <Ice className={"typeIcon"}/> : null}
+          {Type === "Normal" ? <Normal className={"typeIcon"}/> : null}
+          {Type === "Poison" ? <Poison className={"typeIcon"}/> : null}
+          {Type === "Psychic" ? <Psychic className={"typeIcon"}/> : null}
+          {Type === "Rock" ? <Rock className={"typeIcon"}/> : null}
+          {Type === "Steel" ? <Steel className={"typeIcon"}/> : null}
+          {Type === "Water" ? <Water className={"typeIcon"}/> : null}
+        </span>
+    ))
+
     function generateTypes(types : string[]) {
         if(types.length === 1) {
             let color1 = setTypeColor(types[0])
@@ -142,12 +185,7 @@ function Info() {
         <div className="info-container">
             <div className="top-container">
                 <div className="image">
-                    <Card style={{width: "100%", height: "auto"}}>
-                        <Card.Img variant="top" src={pokemon} style={{border: '1px solid rgba(0, 0, 0, 0.5)', borderBottom: "none"}}/>
-                        <Card.Body style={{border: '1px solid rgba(0, 0, 0, 0.5)', padding: 5}}>
-                            <Card.Title style={{textAlign: "center"}}>Normal Pokemon</Card.Title>
-                        </Card.Body>
-                    </Card>
+                    <img src={getFullSize(data ? data.pokemon.pokedexNr : 0)} alt={data?.pokemon.name + " bilde"}></img>
                 </div>
                 <div className="basic-info">
                     <div className="name-number">
@@ -158,8 +196,8 @@ function Info() {
                         <span>{data?.pokemon.species}</span>
                         <span>Generasjon: {data?.pokemon.generation}</span>
                     </div>
-                    <div>
-                        {generateTypes(typesToList(data ? data.pokemon.type1 : "", data ? data.pokemon.type2 : ""))}
+                    <div className="types">
+                        {showTypeEmblem}
                     </div>
                 </div>
             </div>
