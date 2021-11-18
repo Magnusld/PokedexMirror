3080a2dcff0fa14498ed0b679591efd88c8bb4cf
Bjørn Are Odden <bjorao@stud.ntnu.no>
2021-11-18 14:20:44 +0100

    fix(#2): rydde packages

---
+++ backend/src/__tests__/integration/pokemon.ts
@@ -0,0 +1,6 @@
+import {createTestContext} from "./__helpersTestEnvironment";
+
+
+const ctx = createTestContext()
+
+test("")
--- backend/src/__tests__/integration/rating.ts
+++ backend/src/__tests__/integration/rating.ts
@@ -33,12 +33,12 @@ test("CreateRatingMutation should return correct data and persist on server", as
 
 test("ChangeRatingMutation should return correct data and persist on server", async () => {
 
-    const newRating = 3
+    const newRating = 3;
 
-    const beforeChange = await ctx.db.pokemonRating.findMany()
-    expect(beforeChange.length).toBe(1)
-    const preSeededRating = beforeChange[0]
-    expect(preSeededRating.rating).toBe(5)
+    const beforeChange = await ctx.db.pokemonRating.findMany();
+    expect(beforeChange.length).toBe(1);
+    const preSeededRating = beforeChange[0];
+    expect(preSeededRating.rating).toBe(5);
     const result = await ctx.client.request(
         gql`mutation ChangeRating($data: RatingUpdateInput!) {
             ChangeRating(data: $data) {
@@ -60,12 +60,21 @@ test("ChangeRatingMutation should return correct data and persist on server", as
             }
         }
     );
-    expect(result).toMatchSnapshot()
+    expect(result).toMatchSnapshot();
 
-    const afterChange = await ctx.db.pokemonRating.findMany()
-    expect(afterChange.length).toBe(1)
-    expect(afterChange).toMatchSnapshot()
+    const afterChange = await ctx.db.pokemonRating.findMany();
+    expect(afterChange.length).toBe(1);
+    expect(afterChange).toMatchSnapshot();
 
-    const changedRating = afterChange[0]
-    expect(changedRating.rating).toBe(newRating)
+    const changedRating = afterChange[0];
+    expect(changedRating.rating).toBe(newRating);
 });
+
+
+test("DeleteRating should have the correct return value and have change persist on server", async () => {
+    const beforeDelete = await ctx.db.pokemonRating.findMany()
+    expect(beforeDelete.length).toBe(1)
+
+
+});
+
